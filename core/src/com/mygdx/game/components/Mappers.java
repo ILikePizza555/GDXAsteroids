d1bee93a6f6020d2ae93b08e39398a57d34779e5
isaac <avrisaac555@gmail.com>
2015-10-01 14:28:11 -0700

    Added render system and physics system(Box2D)

--- core/src/com/mygdx/game/components/Mappers.java
+++ core/src/com/mygdx/game/components/Mappers.java
@@ -1,8 +1,6 @@
 package com.mygdx.game.components;
 
 import com.badlogic.ashley.core.ComponentMapper;
-import com.mygdx.game.components.actions.MoveBackward;
-import com.mygdx.game.components.actions.MoveForward;
 
 /**
  * Created by isaac on 9/29/15.
@@ -10,9 +8,4 @@ import com.mygdx.game.components.actions.MoveForward;
 public class Mappers {
     public final static ComponentMapper<PhysicsComponent> physicsMapper = ComponentMapper.getFor(PhysicsComponent.class);
     public final static ComponentMapper<RenderComponent> renderMapper = ComponentMapper.getFor(RenderComponent.class);
-    public final static ComponentMapper<KeyboardInputController> kbInputMapper = ComponentMapper.getFor(KeyboardInputController.class);
-
-    public final static ComponentMapper<MoveForward> moveFMapper = ComponentMapper.getFor(MoveForward.class);
-    public final static ComponentMapper<MoveBackward> moveBMapper = ComponentMapper.getFor(MoveBackward.class);
-
 }
